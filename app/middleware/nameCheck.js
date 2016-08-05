427efcded33832ee691d748740b212fda530186e
Chris Sevilleja <chris@scotch.io>
2016-08-05 15:30:34 -0700

    middleware stuff

--- app/middleware/nameCheck.js
+++ app/middleware/nameCheck.js
@@ -7,10 +7,10 @@ module.exports = function(req, res, next) {
   // var user = User.findOne({ username: req.params.username });
 
   // was no user. 404
-  if ( ! user) {
-    res.status(404);
-    return res.send('404 not found');
-  }
+  // if ( ! user) {
+  //   res.status(404);
+  //   return res.send('404 not found');
+  // }
 
   // attaching information to the request
   req.user = {
